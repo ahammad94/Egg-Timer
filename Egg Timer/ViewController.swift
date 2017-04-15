de337c7556844bfd1779a42126a6383e209ab966
Abdelrahman Hammad <ahammad@terpmail.umd.edu>
2017-04-15 18:37:18 -0400

    Egg Timer
    Created a simple egg timer IOS app. Time is defaulted at 210 seconds

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    var timer = Timer()
+    var time = 210
+    
+    func decreaseTimer() {
+        if time > 0{
+            time -= 1
+            timerLabel.text = String(time)
+        }else{
+            timer.invalidate()
+        }
+    }
+    
+    @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playPressed(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)
+    }
+    @IBAction func pausePressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBAction func forwardPressed(_ sender: Any) {
+        time += 10
+        timerLabel.text = String(time)
+    }
+    @IBAction func resetPressed(_ sender: Any) {
+        time = 210
+        timerLabel.text = String(time)
+    }
+    @IBAction func backwardPressed(_ sender: Any) {
+        if time > 10 {
+            time -= 10
+        }
+        timerLabel.text = String(time)
+    }
+   
 }
 
